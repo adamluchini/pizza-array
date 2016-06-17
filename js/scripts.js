bc73811fb09757cfc5c27cdc92ef0f44a59d24e3
Adam Luchini and Adam Sander Luchini <adam@>
2016-06-17 13:14:33 -0700

    pizza app with array

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,63 @@
-function PizzaOrder (quantity, pizzaSize, pizzaToppings) {
-  this.quantity=quantity;
-  this.pizzaSize=pizzaSize;
-  this.pizzaToppings=pizzaToppings
+function Topping(toppingName, price){
+  this.toppingName=toppingName;
 }
 
-PizzaOrder.prototype.calculatePrice=
-function(){
+function PizzaOrder (quantity, pizzaSize) {
+  this.quantity=quantity;
+  this.pizzaSize=pizzaSize;
+  this.toppings=[];
+}
+
+PizzaOrder.prototype.addTopping = function(){
+  var topping=new Topping;
+  this.toppings.push(topping);
+}
+
+PizzaOrder.prototype.calculatePrice=function(){
+  var pizzaTopping=new Topping;
   var orderCost=0;
   if(this.pizzaSize === "small"){
     orderCost += 12;
   } else if(this.pizzaSize==="medium"){
     orderCost += 15;
   } else {
-    orderCost +=20;
+    orderCost +=18;
   }
 
-var finalPrice=orderCost*this.quantity;
-return finalPrice
+  if(this.toppings.length>0){
+    for(var i = 1; i<=this.toppings.length; i++){
+      orderCost +=1;
+    }
+  }
+
+  var finalPrice=orderCost*this.quantity;
+  return finalPrice
 }
 
 $(document).ready(function(){
+  $("#add-topping").click(function() {
+  $("#new-toppings").append('<div class="new-topping">' +
+                                '<div class="form-group">' +
+                                  '<select id="topping">' +
+                                    '<option value="olives">Olives</option>' +
+                                    '<option value="pepperoni">Pepperoni</option>' +
+                                  '</select>' +
+                                '</div>' +
+                              '</div>');
+});
   $("form#order").submit(function(event){
     event.preventDefault();
 
     var inputtedQuantity=parseInt($("input#quantity").val());
     var inputtedSize=$("select#size").val();
-    var inputtedToppings=$("select#toppings").val();
-    var newPizzaOrder=new PizzaOrder(inputtedQuantity, inputtedSize, inputtedToppings);
+
+    var newPizzaOrder=new PizzaOrder(inputtedQuantity, inputtedSize);
+
+    $(".new-topping").each(function(){
+      var inputtedDescription = $ (this).find("select#topping").val();
+      var newTopping = new Topping (inputtedDescription);
+      newPizzaOrder.addTopping();
+    });
 
     console.log(inputtedQuantity);
     var price=newPizzaOrder.calculatePrice();
